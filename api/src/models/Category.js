b770a0b7bfc8bff8e5f9fdcb0bb74b74dbebd739
Esteban Cespedes <ces.esteban@gmail.com>
2020-11-03 12:30:17 -0300

    (api/model):Creacion y relacion del modelo category

--- api/src/models/Category.js
+++ api/src/models/Category.js
@@ -2,7 +2,7 @@ const { DataTypes} = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 
- const Category = sequalize.define('categories', {
+ const Category = sequelize.define('categories', {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
