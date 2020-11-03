795cfb947fc816b3d4bb5f8c1a60d50ac494604d
Maico <maicoloncomilla@gmail.com>
2020-11-03 20:26:39 -0300

    S11-React Router-APP y S15-Detalles del producto segun ID añadido

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
 import ProductCard from './components/product card/ProductCard';
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <ProductCard/>
+      <Route path="/"
+      render={() => <ProductCard/>}
+      />
+      <Route
+      path="/catalogo"
+      render={() => <h1>catalogo</h1>}
+      />
+      <Route
+      path="/product/id"
+      render={() => <h1>Detalles de los Productos segun id</h1>}
+      />
+      {/* Utilizar Route para nuevas rutas */}
     </div>
   );
 }
