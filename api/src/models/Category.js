a0ba10f387163932d32c5ad8538be7688f5854ec
Esteban Cespedes <ces.esteban@gmail.com>
2020-11-03 11:16:22 -0300

    S5-crearModeloCategorias

---
+++ api/src/models/Category.js
@@ -0,0 +1,19 @@
+const { DataTypes} = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+
+ const Category = sequalize.define('categories', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+      description: {
+        type: DataTypes.STRING
+      }
+    })
+
+module.exports = {
+  // defino el modelo
+  Category
+}
