53fb200bb1914344cd9bf2ee33bb11394851252d
Nacho077 <ignaciogimenez70@gmail.com>
2020-11-03 18:33:01 -0300

    formCategories created

---
+++ client/src/component/formCategories/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Styles from './formCategories.module.css'
+
+export default function FormCategories(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
