e9c43584e33710cecb7db33c3676d63a49dda94d
Maico <maicoloncomilla@gmail.com>
2020-11-03 17:52:44 -0300

    'base_creada_S13'

--- client/src/components/catalog/course catalog/CatalogCourse.js
+++ client/src/components/catalog/course catalog/CatalogCourse.js
@@ -3,17 +3,21 @@ import s from './CatalogCourse.module.css';
 import ArrowRightIcon from '@material-ui/icons/ArrowRight';
 
 function CatalogCourse(){
+    const arrayCatalog = [ "Web Development", "Mobile Development", "Game Development", "Entrepreneurship", "Business Analytics & Intelligence", "Digital Marketing", "Graphic Design & Illustration", "IT Certification", "Personal Transformation", "All categories"]
 
     return (
         <div className={s.container_catalog}>
             <div className={s.container_main_catalog}>
                 <p>Most popular</p>
-                <div className={s.container_course_catalog}>
-                    <button>
-                        <p>Web Development</p>
-                        <ArrowRightIcon fontSize="small" />
-                    </button>
-                </div>
+                {arrayCatalog.map( e => (
+                    <div className={s.container_course_catalog}>
+                        <button>
+                            <p>{e}</p>
+                            <ArrowRightIcon fontSize="small" />
+                        </button>
+                    </div>
+                ))}
+                
             </div>
             <div className={s.container_catalog_button}>
                 <button >X</button>
