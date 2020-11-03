d17be0ca549cf4f661f85b4715f048eced56af68
Nacho077 <ignaciogimenez70@gmail.com>
2020-11-03 19:26:15 -0300

    form created

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import FormCategories from './component/formCategories'
 
 function App() {
   return (
     <div className="App">
+      <FormCategories />
       
       
     </div>
--- client/src/component/formCategories/index.js
+++ client/src/component/formCategories/index.js
@@ -4,7 +4,18 @@ import Styles from './formCategories.module.css'
 export default function FormCategories(){
     return(
         <div>
-            
+            <h1 className={Styles.title}>Categories</h1>
+            <form className={Styles.form}>
+                <div className={Styles.section}>
+                    <label className={Styles.label}>Nombre:</label>
+                    <input className={Styles.input}></input>
+                </div>
+                <div className={Styles.section}>
+                    <label className={Styles.label}>Descripcion:</label>
+                    <textarea className={Styles.input, Styles.area}></textarea>
+                </div>
+                <button className={Styles.btn}>Create new categorie</button>
+            </form>
         </div>
     )
 }
