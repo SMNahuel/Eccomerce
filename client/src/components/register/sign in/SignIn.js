f09ba9766caa5ec20495e5b38da4921b6cb58cfb
Maico <maicoloncomilla@gmail.com>
2020-11-04 16:12:53 -0300

    update SignIn (client/src/components/register/sign in/SignIn.js, css) añadido las validaciones del componente Sign In

--- client/src/components/register/sign in/SignIn.js
+++ client/src/components/register/sign in/SignIn.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import s from './SignIn.module.css'
+import React, { useState } from 'react';
+import s from './SignIn.module.css';
 
 function SignIn(){
+    const [ input, setInput ] = useState({
+        username: "",
+        email: "",
+        password: "",
+    })
     return (
         <div className={s.container_signIn}>
             <div className={s.container_title}>
                 <h2>Sign In</h2>
             </div>
-            <div className={s.container_inputs_signIn}>
-                <div className={s.container_inputs}>
-                    <input placeholder="Name..." type="text"/>
+            <form>
+                <div className={s.container_inputs_signIn}>
+                    <div className={s.container_inputs}>
+                        <input 
+                        placeholder="Username..." 
+                        type="text"
+                        pattern="[a-zA-Z_]{6,50}"
+                        id="name"
+                        title="Only letters | min 6 characters | maximum 50 characters"
+                        onChange={e => setInput({...input, username: e.target.value})}
+                        />
+                    </div>
+                    <div className={s.container_inputs}>
+                        <input 
+                        placeholder="Email..." 
+                        type="Email"
+                        id="email"
+                        pattern="[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{1,5}"
+                        onChange={e => setInput({...input, email: e.target.value})}
+                        />
+                    </div>
+                    <div className={s.container_inputs}>
+                        <input 
+                        placeholder="Password..." 
+                        type="password"
+                        id="passwordd"
+                        title="min 7 characters | maximum 20 characters"
+                        pattern="[a-zA-Z0-9_]{7,20}"
+                        onChange={e => setInput({...input, password: e.target.value})}
+                        />
+                    </div>
+                    <div className={s.container_button}>
+                        <button type="submit" onClick={() => console.log(input)}>Sign In</button>
+                    </div>
                 </div>
-                <div className={s.container_inputs}>
-                    <input placeholder="Email..." type="Email"/>
-                </div>
-                <div className={s.container_inputs}>
-                    <input placeholder="Password..." type="password"/>
-                </div>
-                <div className={s.container_button}>
-                        <button>Sign In</button>
-                </div>
-            </div>
+            </form>
         </div>
     )
 }
