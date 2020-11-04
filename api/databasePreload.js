dbaa7641ba4a3f6635d1b534cdf1eb1b92cfa1a9
JavierBalonga <56722632+JavierBalonga@users.noreply.github.com>
2020-11-04 17:53:22 -0300

    utilities:(api\index.js):Hardcodeo una precarga de productos

--- api/databasePreload.js
+++ api/databasePreload.js
@@ -12,7 +12,55 @@ module.exports = ()=> {
             })
         )
     )
-    .then(r => 
-        console.log('categories pre-charged')
+    .then(r => console.log('categories pre-charged'))
+    .then(()=>
+        Promise.all(
+            categories.map((category, index) => 
+                Product.create({
+                    name: category + " I",
+                    description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit conubia ornare mi faucibus, hac gravida quisque dignissim ridiculus proin himenaeos diam et. Nullam risus gravida praesent eu nulla platea per lacinia netus, volutpat facilisis ut ultricies turpi',
+                    price: 100,
+                    stock: 20
+                })
+                .then(product => product.setCategories([index + 1]))
+                .then(res => 
+                    Product.create({
+                        name: category + " II",
+                        description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit conubia ornare mi faucibus, hac gravida quisque dignissim ridiculus proin himenaeos diam et. Nullam risus gravida praesent eu nulla platea per lacinia netus, volutpat facilisis ut ultricies turpi',
+                        price: 120,
+                        stock: 20
+                    })
+                )
+                .then(product => product.setCategories([index + 1]))
+                .then(res => 
+                    Product.create({
+                        name: category + " III",
+                        description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit conubia ornare mi faucibus, hac gravida quisque dignissim ridiculus proin himenaeos diam et. Nullam risus gravida praesent eu nulla platea per lacinia netus, volutpat facilisis ut ultricies turpi',
+                        price: 140,
+                        stock: 20
+                    })
+                )
+                .then(product => product.setCategories([index + 1]))
+                .then(res => 
+                    Product.create({
+                        name: category + " IV",
+                        description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit conubia ornare mi faucibus, hac gravida quisque dignissim ridiculus proin himenaeos diam et. Nullam risus gravida praesent eu nulla platea per lacinia netus, volutpat facilisis ut ultricies turpi',
+                        price: 160,
+                        stock: 20
+                    })
+                )
+                .then(product => product.setCategories([index + 1]))
+                .then(res => 
+                    Product.create({
+                        name: category + " V",
+                        description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit conubia ornare mi faucibus, hac gravida quisque dignissim ridiculus proin himenaeos diam et. Nullam risus gravida praesent eu nulla platea per lacinia netus, volutpat facilisis ut ultricies turpi',
+                        price: 180,
+                        stock: 20
+                    })
+                )
+                .then(product => product.setCategories([index + 1]))
+            )
+        )
     )
+    .then(r => console.log('products pre-charged'))
 }
