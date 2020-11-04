37d319d0641cff291ff2b57256530274f539e2a9
Maico <maicoloncomilla@gmail.com>
2020-11-04 11:54:02 -0300

    (SX5): incluir componentes searchbar, catalog, productcard agregada la ruta de addCategory, Home

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,34 @@
 import React from 'react';
 import './App.css';
+import s from './App.module.css'
 import { Route } from 'react-router-dom';
 import SearchBar from './components/search bar/SearchBar';
 import ProductCard from './components/product card/ProductCard';
 import FormCategories from './components/form category/FormCategories'
-import Catalog from './components/category/Category';
+import Catalog from './components/categories/Categories';
 
 function App() {
   return (
     <div className="App">
-      <Route exact path="/" render={() => (
+      <Route exact path="/" render={() => <h1>ir a /home</h1>}/>
+      <Route exact path="/home" render={() => (
 
-        <div className="container_path_home">
+        <div className={s.container_path_home}>
           <SearchBar />
           <Catalog/>
-          <div className="container_productCard">
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
+          <div className={s.container_productCard}>
+            <>
+              <ProductCard/>
+              <ProductCard/>
+              <ProductCard/>
+            </>
             {/* modo de prueba. */}
           </div>
         </div>
-
       )}
       />
-      <Route path="/category" render={() => <Catalog/>} />
-      <Route path="/category/products/id" render={() => <h1>Detalles de los Productos segun id</h1>} />
+      <Route path="/products/id" render={() => <h1>Detalles de los Productos segun id</h1>} />
+      <Route exact path="/addCategory" render={() => <FormCategories/>}/>
       {/* Falta añadir Rutas de Log In y Sign In */}
       {/* y crear sus componentes */}
       
