34d8a11874eb934c91ae665dbb5c0e4329eda321
JavierBalonga <56722632+JavierBalonga@users.noreply.github.com>
2020-11-07 15:10:53 -0300

    (client/src/...):Mas estilos

--- client/src/components/home/categories/Categories.js
+++ client/src/components/home/categories/Categories.js
@@ -4,18 +4,17 @@ import s from './Categories.module.css'
 
 export default function Categories({categories, onSelect, onClear, selectedCategory}) {
     return(
-        <div className={s.container_catalog}>
+        <div className={s.container}>
             {categories && 
                 categories.map(category => (
-                    <div className={s.container_catalog_button} key={category.id} >
-                        <button 
-                            onClick={category.id === selectedCategory ? onClear : onSelect} 
-                            value={category.id} 
-                            style={category.id === selectedCategory ? {backgroundColor: 'aqua', color: 'black'} : {backgroundColor: '#00B894', color: 'white'}} 
-                        >
-                            {category.name}
-                        </button>
-                    </div>
+                    <button 
+                        key={category.id} 
+                        onClick={category.id === selectedCategory ? onClear : onSelect} 
+                        value={category.id} 
+                        style={category.id === selectedCategory ? {filter: 'hue-rotate(-180deg)'} : {}} 
+                    >
+                        {category.name}
+                    </button>
                 ))
             }
         </div>
