afabe4fc98863f95af48476cd8787e696c2b08e6
Maico <maicoloncomilla@gmail.com>
2020-11-08 20:24:53 -0300

    SX17 (client/src/components/sideBar/sidebarprofile/SideBarProfile.js) añadido un boton para que al darle click a alguna opcion se cierre automaticamente

--- client/src/components/sideBar/sidebarprofile/SideBarProfile.js
+++ client/src/components/sideBar/sidebarprofile/SideBarProfile.js
@@ -28,59 +28,69 @@ function SideBarProfile({ toogleState }){
             <div className={s.container_flex}>
                 <div className={s.container_links}>
                     <Link to="/" className={s.link}>
-                        <div className={s.container_icons_title}>
-                            <div className={s.container_icons}>
-                                <HomeIcon className={s.icon} />
-                                <p>Home</p>
+                        <button onClick={toogleState} className={s.button}>
+                            <div className={s.container_icons_title}>
+                                <div className={s.container_icons}>
+                                    <HomeIcon className={s.icon} />
+                                    <p>Home</p>
+                                </div>
+                                <div>
+                                    <ArrowRightIcon />
+                                </div>
                             </div>
-                            <div>
-                                <ArrowRightIcon />
-                            </div>
-                        </div>
+                        </button>
                     </Link>
                     <Link to="/profile" className={s.link}>
-                        <div className={s.container_icons_title}>
-                            <div className={s.container_icons}>
-                                <PersonIcon className={s.icon} />
-                                <p>Profile</p>
+                        <button onClick={toogleState} className={s.button}>
+                            <div className={s.container_icons_title}>
+                                <div className={s.container_icons}>
+                                    <PersonIcon className={s.icon} />
+                                    <p>Profile</p>
+                                </div>
+                                <div>
+                                    <ArrowRightIcon />
+                                </div>
                             </div>
-                            <div>
-                                <ArrowRightIcon />
-                            </div>
-                        </div>
+                        </button>
                     </Link>
                     <Link to="/cart" className={s.link}>
-                        <div className={s.container_icons_title}>
-                            <div className={s.container_icons}>
-                                <ShoppingCartIcon className={s.icon} />
-                                <p>Cart</p>
+                        <button onClick={toogleState} className={s.button}>
+                            <div className={s.container_icons_title}>
+                                <div className={s.container_icons}>
+                                    <ShoppingCartIcon className={s.icon} />
+                                    <p>Cart</p>
+                                </div>
+                                <div>
+                                    <ArrowRightIcon />
+                                </div>
                             </div>
-                            <div>
-                                <ArrowRightIcon />
-                            </div>
-                        </div>
+                        </button>
                     </Link>
                     <Link to="/product" className={s.link}>
-                        <div className={s.container_icons_title}>
-                            <div className={s.container_icons}>
-                                <ListAltIcon className={s.icon} />
-                                <p>Product</p>
+                        <button onClick={toogleState} className={s.button}>
+                            <div className={s.container_icons_title}>
+                                <div className={s.container_icons}>
+                                    <ListAltIcon className={s.icon} />
+                                    <p>Product</p>
+                                </div>
+                                <div>
+                                    <ArrowRightIcon />
+                                </div>
                             </div>
-                            <div>
-                                <ArrowRightIcon />
-                            </div>
-                        </div>
+                        </button>
                     </Link>
                     <Link to="/catalog" className={s.link}>
-                        <div className={s.container_icons_title}>
-                            <div className={s.container_icons}>
-                                <ViewListIcon className={s.icon} />
-                                <p>Catalog</p>
+                        <button onClick={toogleState} className={s.button}>
+                            <div className={s.container_icons_title}>
+                                <div className={s.container_icons}>
+                                    <ViewListIcon className={s.icon} />
+                                    <p>Catalog</p>
+                                </div>
+                                <div>
+                                    <ArrowRightIcon />
+                                </div>
                             </div>
-                            <div>
-                                <ArrowRightIcon />
-                            </div>
-                        </div>
+                        </button>
                     </Link>
                 </div>
                 <div className={s.container_empty}></div>
@@ -88,37 +98,43 @@ function SideBarProfile({ toogleState }){
                     {/* Esta parte dependera del estado del usuario, si esta logeado, no logeado o si es un admin */}
                     <h4 className={s.title_h4}>Admin</h4>
                     <Link to="/categories" className={s.link}>
-                        <div className={s.container_icons_title}>
-                            <div className={s.container_icons}>
-                                <AddIcon className={s.icon}/>
-                                <p>Create category</p>
+                        <button onClick={toogleState} className={s.button}>
+                            <div className={s.container_icons_title}>
+                                <div className={s.container_icons}>
+                                    <AddIcon className={s.icon} />
+                                    <p>Create category</p>
+                                </div>
+                                <div>
+                                    <ArrowRightIcon />
+                                </div>
                             </div>
-                            <div>
-                                <ArrowRightIcon />
-                            </div>
-                        </div>
+                        </button>
                     </Link>
                     <Link to="/products" className={s.link}>
-                        <div className={s.container_icons_title}>
-                            <div className={s.container_icons}>
-                                <AddIcon className={s.icon}/>
-                                <p>Create | Modify product</p>
+                        <button onClick={toogleState} className={s.button}>
+                            <div className={s.container_icons_title}>
+                                <div className={s.container_icons}>
+                                    <AddIcon className={s.icon} />
+                                    <p>Create | Modify product</p>
+                                </div>
+                                <div>
+                                    <ArrowRightIcon />
+                                </div>
                             </div>
-                            <div>
-                                <ArrowRightIcon />
-                            </div>
-                        </div>
+                        </button>
                     </Link>
                     <Link to="/login" className={s.link}>
-                        <div className={s.container_icons_title}>
-                            <div className={s.container_icons}>
-                                <MeetingRoomIcon className={s.icon} />
-                                <p>Log Out</p>
+                        <button onClick={toogleState} className={s.button}>
+                            <div className={s.container_icons_title}>
+                                <div className={s.container_icons}>
+                                    <MeetingRoomIcon className={s.icon} />
+                                    <p>Log Out</p>
+                                </div>
+                                <div>
+                                    <ArrowRightIcon />
+                                </div>
                             </div>
-                            <div>
-                                <ArrowRightIcon />
-                            </div>
-                        </div>
+                        </button>
                     </Link>
                     
                 </div>
