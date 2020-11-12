0dbcc67dac9fc7a17e265c77e5bcc1258f1d960f
Esteban Cespedes <ces.esteban@gmail.com>
2020-11-11 21:55:48 -0300

    (api/client)AgregouseDispatchButtonyuseSelectorCart

---
+++ client/src/components/home/Cart/Cart.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import useSelector from 'react-redux'
+
+function Cart() {
+
+    const dataProduct = useSelector(state => state.dataProduct)
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Cart;
