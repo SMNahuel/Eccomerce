6db912110b20a52ee6dfee8996fe85865a1c3ced
Esteban Cespedes <ces.esteban@gmail.com>
2020-11-13 17:42:21 -0300

    (api/route)tablaordenesfaltaruta

--- client/src/components/Admin/TableOrders.js
+++ client/src/components/Admin/TableOrders.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { useState } from 'react'
+import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table';
+import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
+import s from './TableOrders.module.css'
 
-function TableOrders(props) {
+function TableOrders() {
+
+    const [, set] = useState();
+
     return (
-        <div>
-            
+        <div className={s.styleTableOrders}>
+            <Table>
+                <Thead>
+                    <Tr>
+                        <Th></Th>
+                        <Th></Th>
+                        <Th></Th>
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    <Tr>
+                        <Td></Td>
+                        <Td></Td>
+                        <Td></Td>
+                    </Tr>
+                </Tbody>
+            </Table>
         </div>
     );
 }
