7da538ebfd1ec5284a41b7063787e8505f7a2c6e
Maico <maicoloncomilla@gmail.com>
2020-11-14 16:39:20 -0300

    SX27:CSS-Login-SignIn (client/src/components) rediseño de login y signup

--- client/src/components/register/sign in/SignIn.js
+++ client/src/components/register/sign in/SignIn.js
@@ -3,7 +3,13 @@ import s from './SignIn.module.css';
 import axios from 'axios';
 import { useDispatch } from 'react-redux';
 import USER from '../../../redux/action-creators'
-import { Redirect } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import FaceRoundedIcon from '@material-ui/icons/FaceRounded';
+import MailOutlineRoundedIcon from '@material-ui/icons/MailOutlineRounded';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import YouTubeIcon from '@material-ui/icons/YouTube';
 
 function SignIn(){
    
@@ -40,46 +46,67 @@ function SignIn(){
     return (
         <div className={s.container_signIn}>
             { registerTrue && <Redirect to="/"/>}
-            <div className={s.container_title}>
-                <h2>Sign In</h2>
+            <div className={s.container_absolute_sign_in}>
+                <div className={s.container_h2}>
+                    <h4>Sign Up with</h4>
+                </div>
+                <div className={s.container_icons}>
+                    <FacebookIcon className={s.icons} />
+                    <GitHubIcon className={s.icons} />
+                    <YouTubeIcon className={s.icons} />
+                </div>
             </div>
             <form>
                 <div className={s.container_inputs_signIn}>
                     <div className={s.container_inputs}>
                         <input 
-                        required
-                        placeholder="Username..." 
                         type="text"
+                        placeholder="Username"
+                        required
                         pattern="[a-zA-Z_]{6,50}"
                         id="name"
+                        maxLength="20"
                         title="Only letters | min 6 characters | maximum 50 characters"
                         onChange={e => setInput({...input, name: e.target.value})}
                         />
+                        <FaceRoundedIcon/>
+                        <label>Username</label>
                     </div>
                     <div className={s.container_inputs}>
                         <input 
-                        placeholder="Email..." 
                         type="Email"
+                        placeholder="Email"
+                        required
                         id="email"
+                        maxLength="60"
                         pattern="[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{1,5}"
                         onChange={e => setInput({...input, email: e.target.value})}
                         />
+                        <MailOutlineRoundedIcon/>
+                        <label>Email</label>
                     </div>
                     <div className={s.container_inputs}>
                         <input 
-                        placeholder="Password..." 
                         type="password"
                         id="passwordd"
+                        placeholder="Password"
+                        required
+                        maxLength="30"
                         title="min 7 characters | maximum 20 characters"
                         pattern="[a-zA-Z0-9_]{7,30}"
                         onChange={e => setInput({...input, password: e.target.value})}
                         />
+                        <LockOutlinedIcon/>
+                        <label>Password</label>
                     </div>
                     <div className={s.container_button}>
-                        <button onClick={register} type="submit">Sign In</button>
+                        <button onClick={register} type="submit">Sign Up</button>
                     </div>
                 </div>
             </form>
+            <div className={s.container_have_account}>
+                <Link to="/login" className={s.container_link}><p>Do you have an account?</p></Link>
+            </div>
         </div>
     )
 }
