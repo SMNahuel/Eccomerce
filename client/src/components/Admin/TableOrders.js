e23c531fbea979290a12124fa9ddffcad3ded6ac
Esteban Cespedes <ces.esteban@gmail.com>
2020-11-14 18:31:05 -0300

    (api/client)creaTablaOrdenesycomponenteOrdenes

--- client/src/components/Admin/TableOrders.js
+++ client/src/components/Admin/TableOrders.js
@@ -1,29 +1,35 @@
+import axios from 'axios';
 import React from 'react';
-import { useState } from 'react'
+import { useState, useEffect} from 'react'
 import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table';
 import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
 import s from './TableOrders.module.css'
 
 function TableOrders() {
 
-    const [, set] = useState();
+    const [orders , setOrders] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:3001/orders')
+        .then(({data})=>setOrders(data))
+    }, []);
 
     return (
         <div className={s.styleTableOrders}>
             <Table>
                 <Thead>
                     <Tr>
-                        <Th></Th>
-                        <Th></Th>
-                        <Th></Th>
+                        <Th>Id Order</Th>
+                        <Th>Status</Th>
                     </Tr>
                 </Thead>
                 <Tbody>
-                    <Tr>
-                        <Td></Td>
-                        <Td></Td>
-                        <Td></Td>
-                    </Tr>
+                        {orders && orders.map(orden => (
+                            <Tr key={orden.id} >
+                                <Td>{orden.id} </Td>
+                                <Td>{orden.state} </Td>
+                            </Tr> 
+                        ))}
                 </Tbody>
             </Table>
         </div>
