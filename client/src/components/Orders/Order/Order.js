35c499d6c16830483991ed09a44473357476457a
JavierBalonga <56722632+JavierBalonga@users.noreply.github.com>
2020-11-16 12:17:09 -0300

    (...):Ver el detalle de una orden y cancelarla desde ordenes

--- client/src/components/Orders/Order/Order.js
+++ client/src/components/Orders/Order/Order.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Tr, Td } from 'react-super-responsive-table';
 /* import s from './Order.module.css'; */
 
-export default function Order({order}) {
+export default function Order({order, onDetail}) {
     const orderMount = () => {
         if (!order.products) return 0
         return order.products.reduce((acc, product) => 
@@ -13,6 +13,7 @@ export default function Order({order}) {
         <Tr key= {order.id}>
             <Td>{order.state}</Td>
             <Td>{orderMount()}</Td>
+            <Td><button onClick={onDetail}>Detalle</button></Td>
         </Tr>
     )
 }
--- client/src/components/Orders/Orders.js
+++ client/src/components/Orders/Orders.js
@@ -4,26 +4,38 @@ import { useDispatch, useSelector } from 'react-redux';
 import api from '../../redux/action-creators';
 import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table';
 import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
-import Order from './Order/Order'
+import Order from './Order/Order';
+import OrderDetail from './OrderDetail/OrderDetail';
 
 export default function Orders() {
     const dispatch = useDispatch()
     const orders = useSelector(state=> state.orders)
     useEffect(() => dispatch(api.getOrders()), []);
 
+    const [orderDetail, setOrderDetail] = useState(null)
+    const handleDetail = order => setOrderDetail(order)
+    const onBack = e => setOrderDetail(null)
+
     return (
-        <div className={s.styleTableOrders}>
-            <Table>
-                <Thead>
-                    <Tr>
-                        <Th>Estado</Th>
-                        <Th>Monto Total</Th>
-                    </Tr>
-                </Thead>
-                <Tbody>
-                    {orders.map && orders.map(order => <Order order={order}/>)}
-                </Tbody>
-            </Table>
-        </div>
+        <>
+            <div className={s.styleTableOrders}>
+                {orderDetail ? 
+                    <OrderDetail order={orderDetail} onBack={onBack}/>
+                    :
+                    <Table>
+                        <Thead>
+                            <Tr>
+                                <Th>Estado</Th>
+                                <Th>Monto Total</Th>
+                                <Th>Detalle</Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            {orders.map && orders.map(order => <Order order={order} onDetail={()=>handleDetail(order)}/>)}
+                        </Tbody>
+                    </Table>
+                }
+            </div>
+        </>
     );
 };
