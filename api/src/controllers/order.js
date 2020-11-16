dab98c1dc839f3bd68e5a73f78d52c4b70d9bf31
JavierBalonga <56722632+JavierBalonga@users.noreply.github.com>
2020-11-16 09:52:01 -0300

    (...):Ahora las compras de productos reserva stock para las mismas

--- api/src/controllers/order.js
+++ api/src/controllers/order.js
@@ -2,20 +2,29 @@ const { Order, Product } = require('../db.js');
 
 module.exports = {
     update: function(cartId, product){
-        Order.findOne({
+        const orderPromise = Order.findOne({
             where:{
                 cartId: cartId,
                 productId: product.id
             }
         })
-        .then(order => {
+        const productPromise = Product.findByPk(product.id)
+        return Promise.all([orderPromise, productPromise])
+        .then(([order, dbProduct]) => {
             if(order){
+                let quantityDifference = order.quantity - product.order.quantity
+                let stockRest = dbProduct.stock + quantityDifference
+                if (stockRest < 0) throw new Error('Not enough stock')
                 if(Number(product.order.quantity) > 0){
-                    return order.update({
-                        quantity: product.order.quantity
-                    })
+                    return Promise.all([
+                        dbProduct.update({stock: stockRest}),
+                        order.update({quantity: product.order.quantity})
+                    ])
                 }else{
-                    return order.destroy()
+                    return Promise.all([
+                        dbProduct.update({stock: dbProduct.stock + order.quantity}),
+                        order.destroy()
+                    ])
                 }
             }
         })
@@ -32,14 +41,42 @@ module.exports = {
         Promise.all([oredrPromise, productPromise])
         .then(([order, dbProduct]) => {
             if(order){
+                let quantityDifference = order.quantity - product.order.quantity
+                let stockRest = dbProduct.stock + quantityDifference
+                if (stockRest < 0) throw new Error('Not enough stock')
                 if(Number(product.order.quantity) > 0){
-                    return order.update({
-                        quantity: product.order.quantity,
-                        price: dbProduct.price
-                    })
+                    return Promise.all([
+                        dbProduct.update({stock: stockRest}),
+                        order.update({
+                            quantity: product.order.quantity,
+                            price: dbProduct.price
+                        })
+                    ])
                 }else{
-                    return order.destroy()
+                    return Promise.all([
+                        dbProduct.update({stock: dbProduct.stock + order.quantity}),
+                        order.destroy()
+                    ])
                 }
+            }
+        })
+    },
+
+    release: function(cartId, product){
+        const orderPromise = Order.findOne({
+            where:{
+                cartId: cartId,
+                productId: product.id
+            }
+        })
+        const productPromise = Product.findByPk(product.id)
+        Promise.all([orderPromise, productPromise])
+        .then(([order, dbProduct]) => {
+            if(order){
+                return Promise.all([
+                    dbProduct.update({stock: dbProduct.stock + order.quantity}),
+                    order.destroy()
+                ])
             }
         })
     }
