a60a3d9fa1df41d4bb3b21d9cccdae2712b45de6
Nacho077 <ignaciogimenez70@gmail.com>
2020-11-19 11:52:06 -0300

    todo conectado

--- client/src/components/home/Product/Review/Review.js
+++ client/src/components/home/Product/Review/Review.js
@@ -21,34 +21,42 @@ export default function Review({reviews, productId}) {
             setReview(reviewsFiltred)
         }
     }
+    const quantityStar = (num) => {
+        return reviews.filter(review => review.qualification === num).length
+    }
+    const numberAverage = () => {
+        let stars = 0
+        reviews.map(review => stars += (review.qualification))
+        return (stars/reviews.length).toFixed(1)
+    }
     return (
         <div className={s.container_review_main}>
             <div className={s.container_review_average}>
                 <div className={s.container_average}>
-                    <p className={s.p_number_average}>4.0</p>
-                    <p className={s.p_stars}>{stars(4)}</p>
+                    <p className={s.p_number_average}>{numberAverage()}</p>
+                    <p className={s.p_stars}>{stars(Math.round(numberAverage()))}</p>
                     <p className={s.p_text}>Average based on {reviews.length} reviews</p>
                 </div>
                 <div className={s.container_progress}>
                     <div className={s.container_label_progress}>
                         <label>5 stars</label>
-                        <progress max="100" value="90"></progress>
+                        <progress max={reviews.length} value={quantityStar(5)}></progress>
                     </div>
                     <div className={s.container_label_progress}>
                         <label>4 stars</label>
-                        <progress max="100" value="85">4 stars</progress>
+                        <progress max={reviews.length} value={quantityStar(4)}>4 stars</progress>
                     </div>
                     <div className={s.container_label_progress}>
                         <label>3 stars</label>
-                        <progress max="100" value="95">3 stars</progress>
+                        <progress max={reviews.length} value={quantityStar(3)}>3 stars</progress>
                     </div>
                     <div className={s.container_label_progress}>
                         <label>2 stars</label>
-                        <progress max="100" value="30">2 stars</progress>
+                        <progress max={reviews.length} value={quantityStar(2)}>2 stars</progress>
                     </div>
                     <div className={s.container_label_progress}>
                         <label>1 stars</label>
-                        <progress max="100" value="0">1 stars</progress>
+                        <progress max={reviews.length} value={quantityStar(1)}>1 stars</progress>
                     </div>
                 </div>
             </div>
