5fd4994c0e258dfa40abd12d25d4c786d3969dbd
Esteban Cespedes <ces.esteban@gmail.com>
2020-11-25 16:10:15 -0300

    (api/client)refactoring

--- client/src/components/header/header right/Cart/Cart.js
+++ client/src/components/header/header right/Cart/Cart.js
@@ -91,26 +91,21 @@ function Cart() {
             <div className={s.container_button_toggle}>
                 <button onClick={onToggleActive}><ShoppingCartIcon fontSize="small" /></button>
             </div>
-
             {active && cart.products ?
-
                 <div className={s.container_absolute_main} ref={ref}>
                     <div className={s.container_main_table_button}>
                         
                         <div className={s.container_button_onUnMount_table}>
                             <button onClick={onUnmount}><HighlightOffIcon fontSize="large" /></button>
-                        </div>
-                        
+                        </div>                        
                         <div className={s.container_table}>
                             <div className={s.containerThead}>
                                 <div>PRODUCTO</div>
                                 <div>SUBTOTAL</div>
-                            </div>
-                            
+                            </div>                            
                             <div className={s.lineStyle}>
                                 <hr />
                             </div>
-
                             <div >
                                 {cart.products && cart.products.map(product =>
                                     (quantities[product.id] !== 0) &&
@@ -165,62 +160,6 @@ function Cart() {
                 </div>
                 :null
             }
-
-
-
-            {/*<div className={s.container_absolute_main} ref={ref}>
-                    <div className={s.container_main_table_button}>
-                        <div className={s.container_button_onUnMount_table}>
-                            <button onClick={onUnmount}><HighlightOffIcon fontSize="large"/></button>
-                        </div>
-                        <div className={s.container_table}>
-                            <table>
-                                <thead className={s.container_thead}>
-                                    <tr>
-                                        <th>Name</th>
-                                        <th>Quantity</th>
-                                        <th>Price</th>
-                                        <th>Delete</th>
-                                    </tr>
-                                </thead>
-                                <tbody className={s.container_tbody}>
-                                    {cart.products && cart.products.map(product =>
-                                        (quantities[product.id] !== 0) &&
-                                        <tr key={product.id}>
-                                            <td>{product.name}</td>
-                                            <td>
-                                                <select
-                                                    onChange={e => chengeQuantity(product.id, e.target.value)}
-                                                    value={quantities[product.id] || product.order.quantity}>
-                                                    {
-                                                        selectorValue && selectorValue.map(value =>
-                                                            <option key={value}>{value}</option>
-                                                        )
-                                                    }
-                                                </select>
-                                            </td>
-                                            <td>${`${product.order.price * quantities[product.id] || product.order.price}`}</td>
-                                            <td onClick={() => onDelete(product.id)}><DeleteForeverIcon className={s.DeleteForeverIcon} /></td>
-                                        </tr>
-                                    )}
-                                </tbody>
-                                <tfoot className={s.container_tfoot}>
-                                    <tr>
-                                        <td>Total:</td>
-                                        <td>{totalQuantity()}</td>
-                                        <td>${totalPrice()}</td>
-                                        <td></td>
-                                    </tr>
-                                </tfoot>
-                            </table>
-                            <div className={s.container_input_button}>
-                                <input type="button" onClick={onBuy} value="Comprar!" />
-                                <input type="button" onClick={onCancel} value="Cancelar Carrito" />
-                            </div>
-                        </div>
-                    </div>
-                                                </div>*/}
-
             {active && !cart.products ?
                 <div className={s.container_absolute_main} ref={ref}>
                     <div className={s.container_main_table_button}>
