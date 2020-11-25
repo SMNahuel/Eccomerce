bec8a20fbba722ca216b600eee2f353572c5655f
JavierBalonga <56722632+JavierBalonga@users.noreply.github.com>
2020-11-25 10:25:15 -0300

    (...): Ahora la caja de comentarios esta funcional pero el estilo murio!

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,9 +2,10 @@ const { Router } = require('express');
 const router = Router();
 
 router.use('/category', require('./category.js'));
-router.use('/question', require('./question.js'));
 router.use('/products', require('./product.js'));
 router.use('/reviews', require('./reviews.js'));
+router.use('/comment', require('./comment.js'));
+router.use('/respond', require('./respond.js'));
 router.use('/orders', require('./orders.js'));
 router.use('/image', require('./image.js'));
 router.use('/user', require('./user.js'));
