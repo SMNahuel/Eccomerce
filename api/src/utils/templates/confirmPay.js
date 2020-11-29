b20a75be2ed2b2fb59404e23c64512bb6c38e470
Maico <maicoloncomilla@gmail.com>
2020-11-29 19:51:22 -0300

    ConfirmPay modificado

--- api/src/utils/templates/confirmPay.js
+++ api/src/utils/templates/confirmPay.js
@@ -1,3 +1,6 @@
 module.exports = function ( data ) {
-    return `<strong>Estimado ${data.name} su pago ha sido procesado por un total de: $${data.price} USD</strong>`
+    return `<div>
+        <h2 style="color: #282C34; text-align:center;">Estimado ${data.name}</h2>
+        <p style="color: #282C34">Su pago ha sido procesado por un total de: <b>$${data.price} USD</b></p>
+    </div>`
 }
