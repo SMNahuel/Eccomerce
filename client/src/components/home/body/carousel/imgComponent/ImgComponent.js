bff4f9ece3182b56ee25ad3986044fe07930191e
JavierBalonga <56722632+JavierBalonga@users.noreply.github.com>
2020-11-30 11:16:10 -0300

    (...):Carusel con Top 5

--- client/src/components/home/body/carousel/imgComponent/ImgComponent.js
+++ client/src/components/home/body/carousel/imgComponent/ImgComponent.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import s from './ImgComponent.module.css'
+import imgNotFound from '../../../../../img/img404.jpg';
+import toStars from '../../../../../utils/toStars';
 
-export default function ImgComponent({ src, name, description, price, stock }) {
+export default function ImgComponent({ product, onDetail }) {
+    const image = product.images[0] ?
+    `${process.env.REACT_APP_API_URL}${product.images[0].url}`:
+    imgNotFound
+    console.log(product);
     return (
-        <div className={s.container_img_button}>
-            <img src={src} alt="slideimg" />
-            <div className={s.container_products_data}>
-                <h1>{name}</h1>
-                <p><b>Price: </b>${price}</p>
-                <p><b>Stock: </b>{stock}</p>
-                <p><b>Descripcion: </b>{description}</p>
-            </div>
+        <div className={s.container} style={{backgroundImage: `url(${image})`}}>
+            <button onClick={onDetail}>GO!</button>
+            <h1>{product.name}</h1>
+            <h2>{toStars(product.qualification_avg)}</h2>
+            <p><b>Price: </b>${product.price}</p>
+            <p><b>Descripcion: </b>{product.description}</p>
         </div>
     )
 
